80e9e3119b504f13bb18a4a4b4203aa0b64a2beb
sauasmast <sdahal@caldwell.edu>
2018-12-03 18:26:58 -0500

    Created a complete program for right answer

--- webscrapper/playgame.py
+++ webscrapper/playgame.py
@@ -6,6 +6,8 @@ class playgame():
     def __init__(self, quotation_list):
         self.quotation_list = quotation_list
         self.random_created = []
+        self.score = 0
+        self.total_question = 0
         self._selectrandom()
 
     def _selectrandom(self):
@@ -22,4 +24,21 @@ class playgame():
             self._askquestion(randomnumber)
 
     def _askquestion(self, number):
-        print(f"{self.quotation_list[number]} \n")
+        print(f"{self.quotation_list[number]['Quotation']} \n")
+        answer = input("Enter the name of the person ")
+        self._check_answer(self.quotation_list[number]['Author'], answer)
+
+    def _check_answer(self, right_ans, user_ans):
+        if right_ans == user_ans:
+            print("Right Answer.")
+            self.total_question += 1
+            self.score += 1
+            cont = input(
+                "Do you want to continue? \n Press y if you want to continue or anything to exit: ")
+            self._continue(cont)
+
+    def _continue(self, cont):
+        if cont == 'y':
+            self._selectrandom()
+        else:
+            print(f"You score was {self.score} out of {self.total_question}.")
